d7ae3fe301eb2ad4eec7589c61c44c010512fe28
EM2O25 <825632@mystma.org>
2023-03-30 15:07:25 -0500

    2

--- main.py
+++ main.py
@@ -2,7 +2,5 @@ def hasL(w):
     ifL = 0
     for i in range(0, len(w)):
         if w[i] == "l":
-            ifL = ifL + 1
             return True
-        else:
-            return False
+    return False
